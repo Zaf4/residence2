0544f3f44fddc0959b91fa396405e6acb61dfc19
Zaf4 <zafermolbio@gmail.com>
2024-03-16 18:18:23 +0100

    modify contact

--- 3_systemVisualize/contact.py
+++ 3_systemVisualize/contact.py
@@ -40,17 +40,24 @@ def main():
     data_files = ['./data/data280.extra',
                  './data/data300.extra',
                  './data/data350.extra',
-                 './data/data400.extra',]
+                 './data/data400.extra',
+                 ]
     
+    print("from file to dataframes...")
     dfs = [data2df(file) for file in data_files]
+    print("select only dna")
     dna_only = [df[(df.type==1)|(df.type==2)] for df in dfs]
-    xyz_arrs = [df[['x','y','z']].to_numpy() for df in dna_only]
+    print("extracting coordinates...")
+    xyz_arrs = [df[['x','y','z']].to_numpy()[::120] for df in dna_only]
+    print("calculating distance matrix...")
     mtxs = [find_distance(arr) for arr in xyz_arrs]
+    print("melting matrices...")
     dfms = [mtx_melt(mtx) for mtx in mtxs]
+    print("plotting...")
     plots = [heatmap(dfm) for dfm in dfms]
 
-    ggsave(gggrid(plots,ncol=2))
-
+    #ggsave(gggrid(plots,ncol=2),filename="./figures/heatmaps.png")
+    ggsave(gggrid(plots,ncol=2)+ggsize(1200,1200),filename="heatmaps.html",path="./figures")
     return
 
 if __name__ == '__main__':
